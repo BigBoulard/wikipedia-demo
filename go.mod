60738ea9af03fae16f0678e19aed459347ddb030
Big_Boulard <maxprocess@gmail.com>
2023-04-10 15:21:07 +0200

    init

--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module github.com/freshman-tech/news-demo
 
 go 1.19
+
+require (
+	github.com/rs/zerolog v1.29.0
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rs/xid v1.4.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"html/template"
@@ -14,6 +15,11 @@ import (
 	"os"
 	"strconv"
 	"time"
+
+	"github.com/freshman-tech/news-demo/logger"
+	"github.com/rs/xid"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/hlog"
 )
 
 var tpl *template.Template
@@ -76,6 +82,24 @@ func (fn handlerWithError) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+}
+
+// loggingResponseWriter because there's no way to access the status code of the response through the http.ResponseWriter type
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+// The loggingResponseWriter struct embeds the http.ResponseWriter type
+// and adds a statusCode property which defaults to http.StatusOK (200) when newLoggingResponseWriter() is called.
+func newLoggingResponseWriter(w http.ResponseWriter) *loggingResponseWriter {
+	return &loggingResponseWriter{w, http.StatusOK}
+}
+
+// updates the value of the statusCode property and calls the WriteHeader() method of the embedded http.ResponseWriter instance.
+func (lrw *loggingResponseWriter) WriteHeader(code int) {
+	lrw.statusCode = code
+	lrw.ResponseWriter.WriteHeader(code)
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) error {
@@ -150,6 +174,17 @@ func searchHandler(w http.ResponseWriter, r *http.Request) error {
 		pageNum = "1"
 	}
 
+	// get the logger from the request context
+	l := zerolog.Ctx(r.Context())
+	// update the logger context to add the "search_query" & "page_num" fields
+	l.UpdateContext(func(c zerolog.Context) zerolog.Context {
+		return c.Str("search_query", searchQuery).Str("page_num", pageNum)
+	})
+
+	// log search query
+	l.Info().
+		Msgf("incoming search query '%s' on page '%s'", searchQuery, pageNum)
+
 	nextPage, err := strconv.Atoi(pageNum)
 	if err != nil {
 		return err
@@ -163,6 +198,9 @@ func searchHandler(w http.ResponseWriter, r *http.Request) error {
 	if err != nil {
 		return err
 	}
+
+	// log response from the Wikipedia API
+	l.Debug().Interface("wikipedia_search_response", searchResponse).Send()
 
 	totalHits := searchResponse.Query.SearchInfo.TotalHits
 
@@ -184,7 +222,82 @@ func searchHandler(w http.ResponseWriter, r *http.Request) error {
 		return err
 	}
 
+	// log success
+	l.Trace().Msgf("search query '%s' succeeded without errors", searchQuery)
+
 	return nil
+}
+
+// logger middleware returns an HTTP handler that logs several details about the HTTP request
+func requestLogger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		l := logger.Get()
+
+		// generated the correlationID
+		correlationID := xid.New().String()
+		// add the correlationID to the request context
+		ctx := context.WithValue(r.Context(), "correlation_id", correlationID)
+		r = r.WithContext(ctx)
+		// update the logger context to include the correlationID
+		l.UpdateContext(func(c zerolog.Context) zerolog.Context {
+			return c.Str("correlation_id", correlationID)
+		})
+		// set the correlationID in the response headers
+		w.Header().Add("X-Correlation-ID", correlationID)
+
+		lrw := newLoggingResponseWriter(w)
+
+		// ensure that the Logger used in the requestLogger() is added to the request context
+		// so that it is accessible in all handler functions.
+		// l.WithContext associates the Logger instance with the provided context
+		// and returns a copy of the context which is immediately used as the argument to r.WithContext(),
+		/// which returns a copy of the request with the new context.
+		r = r.WithContext(l.WithContext(r.Context()))
+
+		// defer func(){}(): ensure that if the handler panics, the request will still be logged so that you can find out what caused the panic
+		defer func() {
+			panicVal := recover()
+			if panicVal != nil {
+				lrw.statusCode = http.StatusInternalServerError // ensure that the status code is updated
+				panic(panicVal)                                 // continue panicking
+			}
+
+			l.
+				Info().
+				Str("method", r.Method).
+				Str("url", r.URL.RequestURI()).
+				Str("user_agent", r.UserAgent()).
+				Dur("elapsed_ms", time.Since(start)).
+				Int("status_code", lrw.statusCode).
+				Msg("incoming request")
+		}()
+
+		next.ServeHTTP(lrw, r)
+	})
+}
+
+func requestLogger2(next http.Handler) http.Handler {
+	l := logger.Get()
+
+	h := hlog.NewHandler(l) // injects l into the request context, returns a handler ( r.WithContext(l.WithContext(r.Context())) )
+
+	accessHandler := hlog.AccessHandler( // scalled after each request
+		func(r *http.Request, status, size int, duration time.Duration) {
+			hlog.FromRequest(r).Info().
+				Str("method", r.Method).
+				Stringer("url", r.URL).
+				Int("status_code", status).
+				Int("response_size_bytes", size).
+				Dur("elapsed_ms", duration).
+				Msg("incoming request")
+		},
+	)
+
+	userAgentHandler := hlog.UserAgentHandler("http_user_agent") // adds the User Agent to the Logger's
+
+	return h(accessHandler(userAgentHandler(next)))
 }
 
 func htmlSafe(str string) template.HTML {
@@ -194,20 +307,24 @@ func htmlSafe(str string) template.HTML {
 var err error
 
 func init() {
+	l := logger.Get()
+
 	tpl, err = template.New("index.html").Funcs(template.FuncMap{
 		"htmlSafe": htmlSafe,
 	}).ParseFiles("index.html")
 	if err != nil {
-		log.Fatal("Unable to initialize HTML templates")
+		l.Fatal().Err(err).Msg("Unable to initialize HTML templates")
 	}
 }
 
 func main() {
+	l := logger.Get()
+
 	fs := http.FileServer(http.Dir("assets"))
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "3000"
+		port = "3001"
 	}
 
 	mux := http.NewServeMux()
@@ -215,7 +332,11 @@ func main() {
 	mux.Handle("/search", handlerWithError(searchHandler))
 	mux.Handle("/", handlerWithError(indexHandler))
 
-	log.Printf("Starting Wikipedia App Server on port '%s'", port)
+	l.Info().
+		Str("port", port).
+		Msgf("Starting Wikipedia App Server on port '%s'", port)
 
-	log.Fatal(http.ListenAndServe(":"+port, mux))
+	l.Fatal().
+		Err(http.ListenAndServe(":"+port, requestLogger(mux))).
+		Msg("Wikipedia App Server Closed")
 }
